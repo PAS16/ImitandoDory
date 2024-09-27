e771862ef0d227ea0268033e0f3b6fe7cab326ef
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-09-27 19:14:44 -0300

    correção do erro

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿String Usuario;
+String traducao;
+
+Console.Clear();
+
+
+Console.WriteLine("traduzindo para baleiês");
+Console.WriteLine("Escreva seu nome.");
+
+Usuario = Console.ReadLine()!;
+
+
+traducao = Usuario.Replace("a", "aaa")
+                .Replace("A", "AAA")
+                .Replace("E", "EEE")
+                .Replace("e", "eee")
+                .Replace("I", "III")
+                .Replace("i", "iii")
+                .Replace("O", "OOO")
+                .Replace("o", "ooo")
+                .Replace("u", "uuu")
+                .Replace("U", "UUU");
+                
+
+Console.WriteLine($"Seu nome em baleiês fica assim {traducao}.");
